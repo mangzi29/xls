66948c80e854cf1163ed23061390fbf820238d60
chenshuaikai <17703751364@163.com>
2020-04-02 19:06:43 +0800

    initialization springboot service support program

---
+++ fx/src/test/java/com/xls/fx/FxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xls.fx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
